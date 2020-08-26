88f4f709a890c17074cd07f0dd95fa03a64799bf
Sophie Wickham <sphwick@gmail.com>
2020-08-26 18:50:25 +0000

    Added product views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_wordpress_templates(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/wordpress_templates.html', context)
+
+
+def all_wordpress_themes(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/wordpress_themes.html', context)
+
+
+def all_squarespace_templates(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/squarespace_templates.html', context)
+
+
+def all_squarespace_plugins(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/squarespace_plugins.html', context)
