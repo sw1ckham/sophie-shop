53e4531e7465cd0d2fa8e252c9f3c88ef0c46579
Sophie Wickham <sphwick@gmail.com>
2020-08-27 16:16:16 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 def view_bag(request):
@@ -10,6 +12,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
 
+    product = Product.objects.get(pk=item_id)
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,6 +21,35 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else: 
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+
+    quantity = request.POST.get('quantity')
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+    
