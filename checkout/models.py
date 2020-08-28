2b9b8fa8240cfc2467efc7f1011635e50940098d
Sophie Wickham <sphwick@gmail.com>
2020-08-28 10:45:57 +0000

    added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.save()
 
     def save(self, *args, **kwargs):
