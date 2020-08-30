26f8f7541cef688bca687e8935b8c747145a9f0e
Sophie Wickham <sphwick@gmail.com>
2020-08-30 09:18:21 +0000

    Added add product url view and template

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('wordpress_themes/', views.all_wordpress_themes, name='wordpress_themes'),
     path('squarespace_templates/', views.all_squarespace_templates, name='squarespace_templates'),
     path('squarespace_plugins/', views.all_squarespace_plugins, name='squarespace_plugins'),
-    path('<product_id>', views.product_detail, name="product_detail")
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
