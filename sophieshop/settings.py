1a81a8197183fdc349dbb3764f86146913eeec0f
Sophie Wickham <sphwick@gmail.com>
2020-08-30 10:45:42 +0000

    deployement part 2

--- sophieshop/settings.py
+++ sophieshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pdbmxwi6d437p6uqlc&rd0_98tk3j*#5i-se(no*9ju4^^+(n4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sw1ckham-sophies-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,13 +122,18 @@ WSGI_APPLICATION = 'sophieshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
